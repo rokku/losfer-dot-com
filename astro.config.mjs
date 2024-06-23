bf9f542bf54ae92d9875a5a19cbdb73111f2c46d
Mark Bridgeman <mark.bridgeman@gmail.com>
2024-06-23 16:38:50 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,5 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://losfer.com.com',
-	base: 'losfer-dot-com',
 	integrations: [mdx(), sitemap()],
 });
